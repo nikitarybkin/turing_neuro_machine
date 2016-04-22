d92aea5ce2816b2377c2f13eabfc8a079e77015d
Nikita Rybkin <rmitone@yandex.ru>
2016-04-23 00:17:52 +0300

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -1,3 +1,5 @@
+__author__ = 'Rybkin & Kravchenko'
+
 from keras.preprocessing import sequence
 from keras.utils import np_utils
 from keras.models import Sequential
@@ -7,29 +9,32 @@ from keras.layers.recurrent import LSTM
 import random
 import numpy as np
 
+
 n = 10
 m = 5
 sample_size = 20000
+value_ambit = (-500, 499)
+norm_factor = value_ambit[1] - value_ambit[0]
 
 
 def random_array_creator(n):
     array = np.zeros((n, 1))
     for i in range(n):
-        array[i] = random.randrange(-50000, 49999)
+        array[i] = random.randrange(value_ambit[0], value_ambit[1])
     return array
 
 
 def arrays_creator(n, m):
     arrays = random_array_creator(n)
     for i in range(m - 1):
-        arrays = np.concatenate((arrays, random_array_creator(n)), 1)
+        arrays = np.concatenate((arrays, random_array_creator(n)))
     return arrays
 
 
-X_train = arrays_creator(n, sample_size)
+X_train = arrays_creator(n, sample_size) / norm_factor
 Y_train = X_train
 for i in range(m - 1):
-    Y_train = np.concatenate((Y_train, X_train), 0)
+    Y_train = np.concatenate((Y_train, X_train), 1)
 model = Sequential()
 # model.add(Embedding(max_features, 128, input_length=maxlen))
 model.add(LSTM(64, return_sequences=True))
@@ -40,6 +45,4 @@ model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam')
 
-model.fit(X_train, Y_train, batch_size=1000, nb_epoch = 1, show_accuracy = True)
-
-# result = model.predict_proba(X)
+model.fit(X_train, Y_train, batch_size=1000, nb_epoch=1, show_accuracy=True)
